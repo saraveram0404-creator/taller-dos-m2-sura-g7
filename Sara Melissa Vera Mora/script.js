a9b0c7f848d9e8c2bc3d620d6c7cfe0db5386efb
sara vera <saraveram0404>
2025-11-19 23:05:22 -0500

    ejercicio 4

--- Sara Melissa Vera Mora/script.js
+++ Sara Melissa Vera Mora/script.js
@@ -26,3 +26,14 @@ let aprobadas = notas.filter(function(nota) {
 });
 
 console.log(aprobadas);
+
+//Ejer 4
+let productos = [{nombre: "Camisa", stock: 10}, {nombre: "Zapatos", stock: 0}, {nombre: "Pantalón", stock: 5}];
+
+let enStock = productos.filter(function(producto){
+    return producto.stock > 0;
+});
+
+console.log(enStock);
+
+
